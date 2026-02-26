016bfa7be050671f9d2635b0e6c169f8139e7071
Matheus-Ribeiro-Jesus <pessoalsenacsp@gmail.com>
2026-02-26 17:28:23 -0300

    Adicionando a vercel e railway

--- src/repositories/loginRepository.ts
+++ src/repositories/loginRepository.ts
@@ -3,9 +3,9 @@ import {Login, dadosLogin} from "../models/loginModel"
 import { QueryResult, ResultSetHeader} from "mysql2"
 
 async function validarLogin(email:string):Promise<Login|null>{
-    const sql = `SELECT clientes.id, clientes.nome, clientes.email, clientes.senha, cargos.nome AS cargo
+    const sql = `SELECT clientes.id, clientes.nome, clientes.email, clientes.senha, roles.nome AS cargos
     FROM clientes 
-    JOIN cargos ON cargos.id = clientes.cargo_id
+    JOIN roles ON roles.id = clientes.cargo_id
     WHERE clientes.email = ? `
     
     const [rows] = await pool.query<Login[]>(sql, [email])
